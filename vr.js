fa92c63b8a4bf41aabd57d214c9f24d93cf29420
Lang Core <52973315+lang-core-org@users.noreply.github.com>
2026-09-16 01:22:20 +0800

    Update vr.js

--- vr.js
+++ vr.js
@@ -97,6 +97,50 @@ class vr{
         }
     }
 
+    /*
+    return Promise with resolved
+    { func draw_left(img), func draw_right(img) }
+    that adapt to width×height or height×width img
+    note: search draw_left,draw_right for more info
+    */
+    static of(width,height){
+        try{
+            let wh = new vr(width,height);
+            let hw = new vr(height,width);
+        }catch(e){
+            return Promise.reject(e);
+        }
+
+        return Promise.resolve(
+            {
+                draw_left:(img) => {
+                    wh.clear();
+                    hw.clear();
+                    return wh.draw_left(img).catch(
+                        (_) => hw.draw_left(img)
+                    );
+                },
+                draw_right:(img) => {
+                    wh.clear();
+                    hw.clear();
+                    return wh.draw_right(img).catch(
+                        (_) => hw.draw_right(img)
+                    );
+                }
+            }
+        );
+    }
+
+
+
+
+
+
+
+
+
+    
+
     //deprecate
     d_right_plus(dx){
         if(Number.isInteger(dx) === true){
